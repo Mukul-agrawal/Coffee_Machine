c4645496d05ae002a1f3216d5a0e4390b140796b
Mukul-agrawal <mukul.agrawal_cs18@gla.ac.in>
2020-04-08 21:49:55 +0530

    create Scanner class object

--- task/src/machine/CoffeeMachine.java
+++ task/src/machine/CoffeeMachine.java
@@ -5,14 +5,6 @@ import java.util.Scanner;
 public class CoffeeMachine {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int water = 200;
-        int milk = 50;
-        int beans = 15;
-        System.out.println("Write how many cups of coffee you will need: ");
-        int cups = scanner.nextInt();
-        System.out.println("For " + cups + " cups of coffee you will need: ");
-        System.out.println(cups * water + " ml of water");
-        System.out.println(cups * milk + " ml of milk");
-        System.out.println(cups * beans + " g of coffee beans");
+
     }
 }
