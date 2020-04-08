a526e9c943a558011dc5f4eb10c9647c0238e8ee
Mukul-agrawal <mukul.agrawal_cs18@gla.ac.in>
2020-04-08 21:51:43 +0530

    print the line

--- task/src/machine/CoffeeMachine.java
+++ task/src/machine/CoffeeMachine.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class CoffeeMachine {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Write how many ml of water the coffee machine has: ");
 
     }
 }
