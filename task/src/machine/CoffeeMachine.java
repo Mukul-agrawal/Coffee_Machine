6f714a91d657f0037c9a092e4b5dcd84f09ff05b
Mukul-agrawal <mukul.agrawal_cs18@gla.ac.in>
2020-04-08 22:26:31 +0530

    print the line

--- task/src/machine/CoffeeMachine.java
+++ task/src/machine/CoffeeMachine.java
@@ -9,6 +9,7 @@ public class CoffeeMachine {
         int water = scanner.nextInt();
         System.out.println("Write how many ml of milk the coffee machine has: ");
         int milk = scanner.nextInt();
+        System.out.println("Write how many grams of coffee beans the coffee machine has: ");
 
     }
 }
