eb02467d6a770910acf822d8f96eb7930b9a256a
Mukul-agrawal <mukul.agrawal_cs18@gla.ac.in>
2020-04-08 21:52:25 +0530

    create water variable

--- task/src/machine/CoffeeMachine.java
+++ task/src/machine/CoffeeMachine.java
@@ -6,6 +6,7 @@ public class CoffeeMachine {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Write how many ml of water the coffee machine has: ");
+        int water = scanner.nextInt();
 
     }
 }
