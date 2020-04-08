5fe23411b6c6e8144da7f2135b4058b5b21f4e65
Mukul-agrawal <mukul.agrawal_cs18@gla.ac.in>
2020-04-08 21:26:17 +0530

    print the third line

--- task/src/machine/CoffeeMachine.java
+++ task/src/machine/CoffeeMachine.java
@@ -4,6 +4,7 @@ public class CoffeeMachine {
     public static void main(String[] args) {
         System.out.println("Starting to make a coffee");
         System.out.println("Grinding coffee beans");
+        System.out.println("Boiling water");
 
 
     }
