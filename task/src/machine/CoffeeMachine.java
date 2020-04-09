fa0a5bd976d6396aeacbeb53743dc856ff09aba5
Mukul-agrawal <mukul.agrawal_cs18@gla.ac.in>
2020-04-09 10:38:32 +0530

    creates variable

--- task/src/machine/CoffeeMachine.java
+++ task/src/machine/CoffeeMachine.java
@@ -5,27 +5,20 @@ import java.util.Scanner;
 public class CoffeeMachine {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("The coffee machine has:");
-        int water = scanner.nextInt();
-        System.out.println(water + " of water");
-        int milk = scanner.nextInt();
-        System.out.println(milk + " of milk");
-        int beans = scanner.nextInt();
-        System.out.println(beans + " of coffee beans");
-        int cups = scanner.nextInt();
-        System.out.println(cups + " of disposable cups");
-        int money = scanner.nextInt();
-        System.out.println(money + " of money");
-        System.out.println("Write action (buy, fill, take):");
-        scanner.nextLine();
+        int water = 400;
+        int milk = 540;
+        int beans = 120;
+        int cups = 9;
+        int money = 550;
+        int loop = 1;
         String option = scanner.nextLine();
         switch (option) {
             case "buy":
-                System.out.println("\"What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino: ");
+                System.out.println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino: ");
                 int n = scanner.nextInt();
                 switch (n) {
                     case 1:
-                        System.out.println("The coffee machine has:\\n" +
+                        System.out.println("The coffee machine has:\n" +
                                 (water - 250) + " of water\n" +
                                 (milk) + " of milk\n" +
                                 (beans - 16) + " of coffee beans\n" +
@@ -48,10 +41,11 @@ public class CoffeeMachine {
                                 (cups - 1) + " of disposable cups\n" +
                                 (money + 6) + " of money");
                         break;
+
                 }
                 break;
             case "fill":
-                System.out.println("\"Write how many ml of water do you want to add:");
+                System.out.println("Write how many ml of water do you want to add:");
                 int extra_water = scanner.nextInt();
                 System.out.println("Write how many ml of milk do you want to add:");
                 int extra_milk = scanner.nextInt();
@@ -74,6 +68,7 @@ public class CoffeeMachine {
                         (beans) + " of coffee beans\n" +
                         (cups) + " of disposable cups\n" +
                         "0 of money");
+
         }
     }
 }
