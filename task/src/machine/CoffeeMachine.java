7e768f037b0087f90923d33a2ad8ff579fce9092
Mukul-agrawal <mukul.agrawal_cs18@gla.ac.in>
2020-04-09 10:40:21 +0530

    create while loop

--- task/src/machine/CoffeeMachine.java
+++ task/src/machine/CoffeeMachine.java
@@ -11,64 +11,68 @@ public class CoffeeMachine {
         int cups = 9;
         int money = 550;
         int loop = 1;
-        String option = scanner.nextLine();
-        switch (option) {
-            case "buy":
-                System.out.println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino: ");
-                int n = scanner.nextInt();
-                switch (n) {
-                    case 1:
-                        System.out.println("The coffee machine has:\n" +
-                                (water - 250) + " of water\n" +
-                                (milk) + " of milk\n" +
-                                (beans - 16) + " of coffee beans\n" +
-                                (cups - 1) + " of disposable cups\n" +
-                                (money + 4) + " of money");
-                        break;
-                    case 2:
-                        System.out.println("The coffee machine has:\n" +
-                                (water - 350) + " of water\n" +
-                                (milk - 75) + " of milk\n" +
-                                (beans - 20) + " of coffee beans\n" +
-                                (cups - 1) + " of disposable cups\n" +
-                                (money + 7) + " of money");
-                        break;
-                    case 3:
-                        System.out.println("The coffee machine has:\n" +
-                                (water - 200) + " of water\n" +
-                                (milk - 100) + " of milk\n" +
-                                (beans - 12) + " of coffee beans\n" +
-                                (cups - 1) + " of disposable cups\n" +
-                                (money + 6) + " of money");
-                        break;
+        while (loop != 0) {
+            System.out.println("Write action (buy, fill, take, remaining, exit):" + "\n");
+            String option = scanner.nextLine();
+            System.out.println();
+            switch (option) {
+                case "buy":
+                    System.out.println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino: ");
+                    int n = scanner.nextInt();
+                    switch (n) {
+                        case 1:
+                            System.out.println("The coffee machine has:\n" +
+                                    (water - 250) + " of water\n" +
+                                    (milk) + " of milk\n" +
+                                    (beans - 16) + " of coffee beans\n" +
+                                    (cups - 1) + " of disposable cups\n" +
+                                    (money + 4) + " of money");
+                            break;
+                        case 2:
+                            System.out.println("The coffee machine has:\n" +
+                                    (water - 350) + " of water\n" +
+                                    (milk - 75) + " of milk\n" +
+                                    (beans - 20) + " of coffee beans\n" +
+                                    (cups - 1) + " of disposable cups\n" +
+                                    (money + 7) + " of money");
+                            break;
+                        case 3:
+                            System.out.println("The coffee machine has:\n" +
+                                    (water - 200) + " of water\n" +
+                                    (milk - 100) + " of milk\n" +
+                                    (beans - 12) + " of coffee beans\n" +
+                                    (cups - 1) + " of disposable cups\n" +
+                                    (money + 6) + " of money");
+                            break;
 
-                }
-                break;
-            case "fill":
-                System.out.println("Write how many ml of water do you want to add:");
-                int extra_water = scanner.nextInt();
-                System.out.println("Write how many ml of milk do you want to add:");
-                int extra_milk = scanner.nextInt();
-                System.out.println("Write how many grams of coffee beans do you want to add:");
-                int extra_beans = scanner.nextInt();
-                System.out.println("Write how many disposable cups of coffee do you want to add:");
-                int extra_cups = scanner.nextInt();
-                System.out.println("The coffee machine has:\n" +
-                        (water + extra_water) + " of water\n" +
-                        (milk + extra_milk) + " of milk\n" +
-                        (beans + extra_beans) + " of coffee beans\n" +
-                        (cups + extra_cups) + " of disposable cups\n" +
-                        (money) + " of money");
-                break;
-            case "take":
-                System.out.println("I gave you $" + (money));
-                System.out.println("The coffee machine has:\n" +
-                        (water) + " of water\n" +
-                        (milk) + " of milk\n" +
-                        (beans) + " of coffee beans\n" +
-                        (cups) + " of disposable cups\n" +
-                        "0 of money");
+                    }
+                    break;
+                case "fill":
+                    System.out.println("Write how many ml of water do you want to add:");
+                    int extra_water = scanner.nextInt();
+                    System.out.println("Write how many ml of milk do you want to add:");
+                    int extra_milk = scanner.nextInt();
+                    System.out.println("Write how many grams of coffee beans do you want to add:");
+                    int extra_beans = scanner.nextInt();
+                    System.out.println("Write how many disposable cups of coffee do you want to add:");
+                    int extra_cups = scanner.nextInt();
+                    System.out.println("The coffee machine has:\n" +
+                            (water + extra_water) + " of water\n" +
+                            (milk + extra_milk) + " of milk\n" +
+                            (beans + extra_beans) + " of coffee beans\n" +
+                            (cups + extra_cups) + " of disposable cups\n" +
+                            (money) + " of money");
+                    break;
+                case "take":
+                    System.out.println("I gave you $" + (money));
+                    System.out.println("The coffee machine has:\n" +
+                            (water) + " of water\n" +
+                            (milk) + " of milk\n" +
+                            (beans) + " of coffee beans\n" +
+                            (cups) + " of disposable cups\n" +
+                            "0 of money");
 
+            }
         }
     }
 }
