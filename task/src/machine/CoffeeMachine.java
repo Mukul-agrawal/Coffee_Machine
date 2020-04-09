efb8cae4578d391fb8264e4dd7c21a6aa1b87870
Mukul-agrawal <mukul.agrawal_cs18@gla.ac.in>
2020-04-09 08:02:35 +0530

    create Scanner class object

--- task/src/machine/CoffeeMachine.java
+++ task/src/machine/CoffeeMachine.java
@@ -5,26 +5,5 @@ import java.util.Scanner;
 public class CoffeeMachine {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Write how many ml of water the coffee machine has: ");
-        int water = scanner.nextInt();
-        System.out.println("Write how many ml of milk the coffee machine has: ");
-        int milk = scanner.nextInt();
-        System.out.println("Write how many grams of coffee beans the coffee machine has: ");
-        int coffee_beans = scanner.nextInt();
-        System.out.println("Write how many cups of coffee you will need: ");
-        int cups = scanner.nextInt();
-        int temp1 = water / 200;
-        int temp2 = milk / 50;
-        int temp3 = coffee_beans / 15;
-        int temp = Math.min(temp1, temp2);
-        int result = Math.min(temp, temp3);
-        if (result == cups) {
-            System.out.println("Yes, I can make that amount of coffee");
-        } else if (result < cups) {
-            System.out.println("No, I can make only " + result + " cup(s) of coffee");
-        } else if (result > cups) {
-            System.out.println("Yes, I can make that amount of coffee (and even " + (result - cups) + " more than that)");
-        }
-
     }
 }
